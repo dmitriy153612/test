4ebb418112ab30160ca3650a23bd77395293573a
Дмитрий Щербаков <dmitrii_s_qschool_2@qsoft.ru>
2024-11-23 18:20:05 +0300

    input

---
+++ src/components/AppInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <input type="text" v-model="updatedModelValue">
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  modelValue: { type: String, required: true },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const updatedModelValue = computed({
+  get: () => props.modelValue,
+  set: (newValue) => emit('update:modelValue', newValue.replace(/\D/g,''))
+})
+
+// function addSpaces(str) {
+//   const reverset = [...str].reverse()
+//   const resultArr = []
+
+//   for (let i = 0; i < reverset.length; i++) {
+//     if ((i + 1) % 3 === 0) {
+//       resultArr.push(reverset[i], ' ')
+//     } else {
+//       resultArr.push(reverset[i])
+//     }
+//   }
+//   return resultArr.reverse().join('')
+// }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <app-input v-model="input"/>
+    {{ input }}
+  </div>
+</template>
+
+<script setup>
+import AppInput from '@/components/AppInput.vue';
+import { ref } from 'vue';
+
+const input = ref('')
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
